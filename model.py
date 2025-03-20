8f711f66c50bd9d68165f82227ebea9e6642bd2d
yusuf6815 <eymenn2012y@gmail.com>
2025-03-20 20:37:52 +0300

    Create model.py

---
+++ model.py
@@ -0,0 +1,19 @@
+from keras.models import load_model
+from PIL import Image, ImageOps
+
+def get_class(model_path, labels_path, Image_path ):
+    np.set_printoptions(supress=True)
+    model=load_model(model_path, compile=false)
+    class_name=open(labels_path, "r", encoding="utf-8"readlines)
+    data= np.ndarray(shape=(1,224,224,3), dtype=np.float32)
+    image= Image.open(image_path).convert("RGB")
+    size=(224,224)
+    image=ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    image_array= np.asarray(image)
+    normalized_image_array=(image_array.astype(np.float32)/127.5)-1
+    data[0]=normalized_image_array
+    prediction=model.predict(data)
+    index= np.argmax(prediction)
+    class_name= class_name(index)
+    confidence_score=prediction[0][index]
+    return(class_name[2:], confidence_score)
